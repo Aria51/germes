2ff258a5f6f7d584bd9bf927cd9be75315a62582
Aria51 <aria51@yandex.ru>
2016-02-10 00:16:42 +0600

    1

--- nova_m2.c
+++ nova_m2.c
@@ -37,133 +37,133 @@ Data Stack size         : 256
 #include <1wire.h>
 #include <ds18b20.h>
 #include <delay.h>
+
 //Глобальные переменные
+
 char input_char;
 int input_count=0;
 int oldTemper=0;
 int rx_bufer_count=10;
 int rx_bufer_index=0;
 char rx_buf[rx_bufer_count];
+
 //Инициализация UART
+
 void intiUart(void)
 {
-UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
-UCSRB=(1<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
-UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
-UBRRH=0x00;
-UBRRL=0x33;
+	UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+	UCSRB=(1<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+	UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+	UBRRH=0x00;
+	UBRRL=0x33;
 }
 //Инициализация портов
 void init_led(void)
 {
-PORTB.0=0;
-DDRB.0=1;
+	PORTB.0=0;
+	DDRB.0=1;
 }
 
 //Инициализация датчика температуры
 void initTemter(void)
 {
-w1_init();
+	w1_init();
 }
 void returnText()
 {
-int i;
-UDR='_';
-for( i=0; i<=rx_bufer_index;i++)
-{
-UDR=rx_buf[i];
-delay_ms(10);
-}
-rx_bufer_index=0;
+	int i;
+	UDR='_';
+	for( i=0; i<=rx_bufer_index;i++)
+	{
+		UDR=rx_buf[i];
+		delay_ms(10);
+	}
+	rx_bufer_index=0;
 }
 
 
 
 void putTemper(int temper)
 {
-char temp_buf[10];
-int i;
+	char temp_buf[10];
+	int i;
                
-    sprintf(temp_buf,"t=%i.%u\xdfC",temper, temper%1);             
-    for(i=0; i<sizeof(temp_buf);i++)
-    {
-         UDR=temp_buf[i];
-         delay_ms(10);
-    }
-//    UDR='\n\r';
+	sprintf(temp_buf,"t=%i.%u\xdfC",temper, temper%1);             
+	for(i=0; i<sizeof(temp_buf);i++)
+	{
+		UDR=temp_buf[i];
+		delay_ms(10);
+	}
 }
 
 int getTemper(void)
 {
-int temper;
-temper=ds18b20_temperature(0);
-if(temper>1000)
-    {
-        temper=4096-temper;
-        temper=-temper;
-    } 
- return temper;
+	int temper;
+	temper=ds18b20_temperature(0);
+	if(temper>1000)
+	{
+        	temper=4096-temper;
+	        temper=-temper;
+	} 
+	return temper;
 }
 void uartAnalis()
 {
-if(rx_buf[0]=='2')
-{ 
- putTemper(getTemper());
-}
+	if(rx_buf[0]=='2')
+	{ 
+		putTemper(getTemper());
+	}
 }
 
 //нагрузка 1
 void led_on_off(void)
 {
-    PORTB.0=~PORTB.0;
+	PORTB.0=~PORTB.0;
 }
 //Прерывание UART
 interrupt [USART_RXC] void usart_rx_isr(void)
 {
-char status,data;
-status=UCSRA;
-data=UDR;
-if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
-   {
-       if((data==0x00)&&(rx_bufer_index!=0)) uartAnalis();
-       if(data!=0x00)
-       {
-        rx_buf[rx_bufer_index]=data;
-        rx_bufer_index++;
-         
-       //rx_bufer_count
-        if(rx_bufer_index==rx_bufer_count)  returnText();
-       }
-//       input_char=data;
-//       input_count=1;    
-   }
-  }
+	char status,data;
+	status=UCSRA;
+	data=UDR;
+	if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
+		{
+			if((data==0x00)&&(rx_bufer_index!=0)) uartAnalis();
+		        if(data!=0x00)
+	        	{
+				rx_buf[rx_bufer_index]=data;
+			        rx_bufer_index++;       
+			        if(rx_bufer_index==rx_bufer_count)  returnText();
+		        }
+
+   		}
+}
 
 void main(void)
 {
-intiUart();
-initTemter();
-init_led();
-#asm("sei");
-putsf("Hello, world!"); 
-while (1)
-      {              
-          if((input_char=='1')&&(input_count==1))
-          {
-               led_on_off();
-               input_count=0;         
-          }            
-           if((input_char=='0')&&(input_count==1))
-          {
-            led_on_off();
-            input_count=0;                
-            putTemper(getTemper());            
-          }
-          if(oldTemper!=getTemper())
-          {
-            oldTemper=getTemper();
-            putTemper(oldTemper);                
-          }
-      }
+	intiUart();
+	initTemter();
+	init_led();
+	#asm("sei");
+	putsf("Hello, world!"); 
+	while (1)
+		{              
+			if((input_char=='1')&&(input_count==1))
+		        {
+				led_on_off();
+		                input_count=0;         
+	                }
+           		if((input_char=='0')&&(input_count==1))
+	                {
+				led_on_off();
+				input_count=0;                
+				putTemper(getTemper());            
+			}
+			if(oldTemper!=getTemper())
+			{
+				oldTemper=getTemper();
+				putTemper(oldTemper);                
+			}
+		}
 }
 
